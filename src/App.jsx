12455884ef70918017bb5dc2dc5d061c07c3b7a3
Raghavendra-k-v-24 <raghavendrakv24@gmail.com>
2025-07-22 22:01:51 +0530

    added poc banner

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import { Toaster } from "@/components/ui/sonner";
 function App() {
   return (
     <div className="h-screen w-full flex flex-col ">
-      {/* <div className="w-full bg-yellow-400 text-black text-center py-1 font-semibold tracking-wide">
+      <div className="w-full bg-yellow-400 text-black text-center py-1 font-semibold tracking-wide">
         🚧 Proof of Concept - Not for Production Use 🚧
-      </div> */}
+      </div>
 
       <Header />
       <Titlebar />
